5909c9f103d0d31df953b6fb27bf96c74a94360d
111522203005 <22203005@rmd.ac.in>
2024-01-20 19:42:18 +0530

    allset

--- turfapp/models.py
+++ turfapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Booking(models.Model):
     name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
     date = models.DateField()
     session = models.CharField(max_length=50)
     mobile_number = models.CharField(max_length=10)
