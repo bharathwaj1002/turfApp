c43295d59ae002b508df2416bad6ebe67c831d3e
111522203005 <22203005@rmd.ac.in>
2024-01-20 20:35:15 +0530

    allset

--- turfproject/settings.py
+++ turfproject/settings.py
@@ -56,8 +56,6 @@ CSRF_TRUSTED_ORIGINS = [
     "https://psm-sports-village-production.up.railway.app"
 ]
 
-LOGIN_REDIRECT_URL = 'index'
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -65,8 +63,8 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'b22203021@gmail.com'
 EMAIL_HOST_PASSWORD = 'wannacumdamapla'
 
-DEFAULT_FROM_EMAIL = 'your_email@gmail.com'
-SERVER_EMAIL = 'your_email@gmail.com'
+DEFAULT_FROM_EMAIL = 'b22203021@gmail.com'
+SERVER_EMAIL = 'b22203021@gmail.com'
 
 ROOT_URLCONF = 'turfproject.urls'
 
