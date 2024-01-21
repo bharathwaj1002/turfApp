09ab16590a5f0f4a445c12b65a0a416a03089f55
111522203005 <22203005@rmd.ac.in>
2024-01-21 16:33:50 +0530

    allset

--- turfapp/views.py
+++ turfapp/views.py
@@ -13,6 +13,8 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.core.files import File
 import qrcode
+from pyotp import TOTP
+from brevo import SibApiV3Sdk
 
 # Create your views here.
 def index(request):
@@ -48,12 +50,38 @@ def check_availability(request):
 def confirm_booking(request, name, date, session, mobile_number, email):
     selected_date = datetime.strptime(date, '%Y-%m-%d').date()
     booking = None
+    otp_error = False  # Flag to indicate OTP validation errors
 
     if request.method == 'POST':
-        booking = Booking.objects.create(name=name, date=selected_date, session=session, mobile_number=mobile_number, email=email)
-        return redirect('complete_booking', booking_id=booking.pk)
+        entered_otp = request.POST.get('otp')
+        stored_otp = request.session.get('otp')
 
-    return render(request, 'confirm_booking.html', {'booking': booking, 'name': name, 'date': date, 'session': session, 'mobile_number': mobile_number, 'email':email})
+        if entered_otp == stored_otp:
+            # OTP is valid, proceed with booking creation
+            booking = Booking.objects.create(name=name, date=selected_date, session=session, mobile_number=mobile_number, email=email)
+            del request.session['otp']  # Remove OTP from session after verification
+            return redirect('complete_booking', booking_id=booking.pk)
+        else:
+            # Invalid OTP, display an error message
+            otp_error = True
+
+    # If not a POST request or OTP validation failed, render the confirmation page
+    return render(request, 'confirm_booking.html', {
+        'booking': booking,  # Pass booking object if already created
+        'name': name, 'date': date, 'session': session, 'mobile_number': mobile_number, 'email': email,
+        'otp_error': otp_error  # Pass the error flag for template rendering
+    })
+
+
+
+
+def store_otp(request):
+  if request.method == 'POST':
+    otp = request.POST.get('otp')
+    request.session['otp'] = otp
+    return JsonResponse({'success': True})
+  else:
+    return JsonResponse({'success': False}, status=405)
 
 
 
@@ -76,7 +104,7 @@ def complete_booking(request, booking_id):
     qr_img.save(qr_img_io, format='PNG')
     qr_img_io.seek(0)
 
-    # Create and send email
+    # Create and email
     subject = 'Booking Confirmation'
     to_email = [booking.email]  # Replace with the actual field name storing the customer's email
 
@@ -129,12 +157,9 @@ def view_bookings(request):
 
 
 
-@login_required
-def admin_home(request):
-    if request.user.is_staff:
-        return redirect('index')
-    else:
-        return redirect('index')
+@user_passes_test(is_superuser)
+def turf_incharge_verification(request):
+    return render(request,'turf_incharge_verification.html')
     
     
     
