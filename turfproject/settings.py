7874d901f70783018740a2934b3bdb2faceb0728
111522203005 <22203005@rmd.ac.in>
2024-03-01 17:59:55 +0530

    allset

--- turfproject/settings.py
+++ turfproject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pkw#b7dcc$4^ej#qb!4lf*&z4b9qyjxgnyz1rdq=!pbiz==a(9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -149,5 +149,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-RAZORPAY_API_KEY = 'rzp_test_7nwMeqivWvFiMx'
-RAZORPAY_SECRET = 'hVcCn5m0h2b72KJWeeOOggFY'
+RAZORPAY_API_KEY = 'rzp_test_nhRP626DA6pByT'
+RAZORPAY_SECRET = 'sMbe4NZBNnCO4AIkclNjTBJX'
